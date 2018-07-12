6ab0ff88b4aed6f75ed783f9228026559228836c
minsung Joh <jms5049@naver.com>
2018-07-12 18:30:45 +0900

    test

---
+++ BurgerKing/testShop.java
@@ -0,0 +1,20 @@
+﻿
+import java.util.Scanner;
+
+public class testShop {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Burgerking bk = null;
+		Scanner input = new Scanner(System.in);
+		int i= 0;
+		String className = args[0];
+		
+		
+		i = input.nextInt();
+		bk.purchase(i);
+		bk.bill();
+
+	}
+
+}
